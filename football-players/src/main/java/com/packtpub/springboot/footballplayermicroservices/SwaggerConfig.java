19b363716c76fb8eb9d2ab1c555baca64c15666b
simon-towler <simon.towler@icloud.com>
2021-01-10 17:11:40 +0000

    Added custom meta data to Swagger UI

--- football-players/src/main/java/com/packtpub/springboot/footballplayermicroservices/SwaggerConfig.java
+++ football-players/src/main/java/com/packtpub/springboot/footballplayermicroservices/SwaggerConfig.java
@@ -3,12 +3,17 @@ package com.packtpub.springboot.footballplayermicroservices;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+
+// http://localhost:8080/swagger-ui/index.html
 
 @Configuration
 @EnableWebMvc
@@ -20,7 +25,8 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .apis(RequestHandlerSelectors.basePackage(
                         "com.packtpub.springboot.footballplayermicroservices.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(metaData());
     }
 
     @Override
@@ -29,6 +35,18 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot REST API")
+                .description("\"Spring Boot REST API for Football Player Microservices\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("\"http://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(new Contact("Mauro Vocale", "https://github.com/Hands-on-MSA-JakartaEE",
+                        "mauro.vocale@gmail.com"))
+                .build();
     }
 }
 
